7b98f551afa18c7d670820fc0c4dd4e699ea821c
lsilvpin <lsilvpin@gmail.com>
2026-05-31 22:36:52 -0300

    feat(infra): adiciona 6 novos comandos CLI (install, fmt, lint, test, cover, validate) e atualiza IoC

--- app/infra/init/ioc_init.py
+++ app/infra/init/ioc_init.py
@@ -9,6 +9,12 @@ from core.application.push_image_use_case import PushImageUseCase
 from core.application.update_image_tag_use_case import UpdateImageTagUseCase
 from core.application.sync_argocd_use_case import SyncArgoCDUseCase
 from core.application.resolve_app_names_use_case import ResolveAppNamesUseCase
+from core.application.install_use_case import InstallUseCase
+from core.application.fmt_use_case import FmtUseCase
+from core.application.lint_use_case import LintUseCase
+from core.application.test_use_case import TestUseCase
+from core.application.cover_use_case import CoverUseCase
+from core.application.validate_use_case import ValidateUseCase
 from infra.init.cli_init import CliInit
 
 
@@ -26,6 +32,19 @@ class IocInit:
         update_uc = UpdateImageTagUseCase(github_client=github_client, logger=logger)
         sync_uc = SyncArgoCDUseCase(argocd_client=argocd_client, logger=logger)
         resolve_uc = ResolveAppNamesUseCase(logger=logger)
+        install_uc = InstallUseCase(logger=logger)
+        fmt_uc = FmtUseCase(logger=logger)
+        lint_uc = LintUseCase(logger=logger)
+        test_uc = TestUseCase(logger=logger)
+        cover_uc = CoverUseCase(logger=logger)
+        validate_uc = ValidateUseCase(
+            install_uc=install_uc,
+            fmt_uc=fmt_uc,
+            lint_uc=lint_uc,
+            test_uc=test_uc,
+            cover_uc=cover_uc,
+            logger=logger,
+        )
 
         return CliInit(
             parse_uc=parse_uc,
@@ -34,5 +53,11 @@ class IocInit:
             update_uc=update_uc,
             sync_uc=sync_uc,
             resolve_uc=resolve_uc,
+            install_uc=install_uc,
+            fmt_uc=fmt_uc,
+            lint_uc=lint_uc,
+            test_uc=test_uc,
+            cover_uc=cover_uc,
+            validate_uc=validate_uc,
             logger=logger,
         )
